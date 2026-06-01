3a013dbc4112aa88747f059edffb8829a98940a6
Abdhesh-0001 <abdheshkumaryadav851@gmail.com>
2026-06-01 12:07:02 +0000

    add new features to Ara

--- Ara_web.py
+++ Ara_web.py
@@ -189,7 +189,7 @@ if user_input:
     if weather_info:
         full_message += f"\n\nWeather info: {weather_info}"
     
-    # Send to AI for response
+    # Send to AI for response (include latest web/search/weather context)
     st.session_state.messages.append({"role": "user", "content": full_message})
     
     try:
