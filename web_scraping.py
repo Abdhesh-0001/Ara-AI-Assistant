f8ac10cffeea9fb36aa02b93ea0c20b121ae2a01
Abdhesh-0001 <abdheshkumaryadav851@gmail.com>
2026-09-15 13:53:00 +0000

    add new features to ara

--- web_scraping.py
+++ web_scraping.py
@@ -21,5 +21,5 @@ for product in products:
     print(f"  - {product.text}")
 
 # Find by ID (single result)
-featured = soup.find_all(id_='featured')
+featured = soup.find(id_='featured')
 print(f"\nFeatured: {featured.text}")
