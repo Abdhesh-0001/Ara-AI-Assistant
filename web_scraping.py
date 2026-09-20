ff72c6f644214fb7d0bfc5dc03c22e8f64cd88ca
Abdhesh-0001 <abdheshkumaryadav851@gmail.com>
2026-09-20 07:24:36 +0000

    add new featires to ara

--- web_scraping.py
+++ web_scraping.py
@@ -1,36 +1,17 @@
+import requests
 from bs4 import BeautifulSoup
 
-html = """
-<html>
-  <div class="container">
-    <div class="product">iPhone 15</div>
-    <div class="product featured">Galaxy S24</div>
-    <div class="product featured">Galaxy S29</div>
-    <div class="product">Pixel 8</div>
-    <p id="discount">20% OFF</p>
-  </div>
-</html>
-"""
+url = "http://quotes.toscrape.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
-soup = BeautifulSoup(html, 'html.parser')
+# Find all quotes using CSS selector
+quotes = soup.select('.quote')
 
-# Test these selectors:
-print("1. All products:")
-products = soup.select('.container>.product')
-for p in products:
-    print(f"  {p.text}")
-
-print("\n2. Only featured product:")
-featured = soup.select('.product.featured')
-for f in featured:
-    print(f"  {f.text}")
-
-print("\n3. Discount text:")
-discount = soup.select('#discount')
-for d in discount:
-    print(f"  {d.text}")
-
-print("\n4. First product only:")
-first = soup.select('.product')[1:]
-for f in first:
-    print(f"  {f.text}")
+for quote in quotes[:3]:  # First 3 only
+    text = quote.select_one('.text').text
+    author = quote.select_one('.author').text
+    
+    print(f"Quote: {text}")
+    print(f"Author: {author}")
+    print("---")
