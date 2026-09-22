57463273f93d2b59cf854f79d89d01e7faf4d18e
Abdhesh-0001 <abdheshkumaryadav851@gmail.com>
2026-09-22 06:01:00 +0000

    add new features to ara

--- web_scraping.py
+++ web_scraping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-url = "https://news.ycombina.com/"
+url = "https://news.ycombinator.com/"
 
 try:
     response = requests.get(url, timeout=5)
